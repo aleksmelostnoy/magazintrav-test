9c648706af93d66b2e8010d07394e8d86aefa326
mochaches <mentos_vist@mail.ru>
2019-09-17 23:28:10 +0300

    Проверка вклдаки Вакансии

--- src/test/java/steps/VacancyCheck.java
+++ src/test/java/steps/VacancyCheck.java
@@ -11,61 +11,24 @@ public class VacancyCheck extends BaseTest {
 
     Vacancy vacancy;
 
-
     @Допустим("^пользователь жмет на вклдаку 'О компании'$")
 
     public void пользователь_жмет_на_вклдаку_О_компании() throws Throwable {
         driver.get("https://magazintrav.ru");
 
-        //Thread.sleep(1000);
-
         vacancy = new Vacancy(driver);
         vacancy.companyBtnClick();
     }
-
     @И("^в появившемся списке выбирает 'Вакансии'$")
     public void в_появившемся_списке_выбирает_Вакансии() throws Throwable {
         vacancy.vacancyBtnClick();
     }
-
     @Тогда("^открывается страница Вакансий$")
     public void открывается_страница_Вакансий() throws Throwable {
         vacancy.chechingString();
     }
-
     @Тогда("^можно проверить список$")
     public void можно_проверить_список() throws Throwable {
         vacancy.checkingVacancyBlock();
     }
-
 }
-//
-//    @Test
-//    public void testSeach() throws InterruptedException {
-//        startDriver();
-//        driver.get("https://magazintrav.ru");
-//
-//        Vacancy vacancy = PageFactory.initElements(driver, Vacancy.class);
-//        vacancy.stringSeachClick();
-//        Thread.sleep(1000);
-//        vacancy.enterAndSeachingWord();
-//        Thread.sleep(1000);
-//        vacancy.checkSeaching();
-//        Thread.sleep(1000);
-//    }
-//
-//    @Test
-//    public void testOnlineMessanger() throws InterruptedException {
-//        startDriver();
-//        driver.get("https://magazintrav.ru");
-//
-//        Vacancy vacancy = PageFactory.initElements(driver, Vacancy.class);
-//
-//        vacancy.messangerClick();
-//        Thread.sleep(1000);
-//        vacancy.selectMess();
-//        Thread.sleep(1000);
-//        vacancy.startChat();
-//        Thread.sleep(1000);
-//    }
-//}
