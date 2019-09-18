08ccb3f8c8657b82b62197bb2203ebfae15d14f5
Andi3z <gremo77t@gmail.com>
2019-09-18 10:53:31 +0300

    Thank test complete

--- src/main/java/pageobjects/MainPage.java
+++ src/main/java/pageobjects/MainPage.java
@@ -3,21 +3,47 @@ package pageobjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import java.util.List;
 
-public class MainPage extends BasePage {
-
-    @FindBy(xpath = "//*[@href=\"/vyiskazat_blagodarnost\"]/*[@alt=\"Высказать Благодарность\"]")
-
-    WebElement thankButton;
+public class MainPage extends BasePage{
 
     public MainPage(WebDriver driver) {
         super(driver);
-
     }
+
+    @FindBy(xpath = "//*[@href=\"/vyiskazat_blagodarnost\"]/*[@alt=\"Высказать Благодарность\"]")
+    WebElement thankButton;
+
+    @FindBy(xpath = "//input[@type='search']")
+    private WebElement searchLabel;
+
+    @FindBy(xpath = "//input[@id='q']")
+    private WebElement bigSearchInputField;
+
+    @FindBy(xpath = "//div[contains(@class, 'multi-content')]/span")
+    private List<WebElement> multiContent;
+
+
+
 
     public ThankPage openThankPage() {
         thankButton.click();
         return new ThankPage(driver);
+    }
+
+    public void typeWordForSearch(String word) {
+        searchLabel.click();
+        bigSearchInputField.sendKeys(word);
+    }
+
+    public boolean isItemPresentByTitle(String title) {
+        for (WebElement item : multiContent) {
+            if (item.getText().contains(title)) {
+                return true;
+            }
+        }
+        return false;
+
     }
 
 }
