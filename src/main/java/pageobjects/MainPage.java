024eacc59632c669978ff610ae6f98d827858e2a
Andi3z <gremo77t@gmail.com>
2019-09-18 10:59:11 +0300

    # Conflicts: #	src/main/java/pageobjects/MainPage.java

--- src/main/java/pageobjects/MainPage.java
+++ src/main/java/pageobjects/MainPage.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import java.util.List;
 
-public class MainPage extends BasePage{
+public class MainPage extends BasePage {
+
+
 
     public MainPage(WebDriver driver) {
         super(driver);
